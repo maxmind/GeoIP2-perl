0f272db2a3fc0a17af11426cb1eff49648185d88
Dave Rolsky <autarch@urth.org>
2013-03-13 11:26:27 -0500

    Add CityISPOrg model

---
+++ lib/GeoIP2/Model/CityISPOrg.pm
@@ -0,0 +1,16 @@
+package GeoIP2::Model::CityISPOrg;
+
+use strict;
+use warnings;
+
+use GeoIP2::Types qw( HashRef object_isa_type );
+use Sub::Quote qw( quote_sub );
+
+use Moo;
+
+with 'GeoIP2::Role::Model';
+
+__PACKAGE__->_define_attributes_for_keys(
+    qw( city continent country location region traits ));
+
+1;
