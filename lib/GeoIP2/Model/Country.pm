66508b3fde81c3c24f43113c61ad160c86665994
Dave Rolsky <autarch@urth.org>
2013-03-25 13:56:07 -0500

    Add docs for the Model classes

--- lib/GeoIP2/Model/Country.pm
+++ lib/GeoIP2/Model/Country.pm
@@ -14,3 +14,54 @@ __PACKAGE__->_define_attributes_for_keys(
     qw( continent country registered_country traits ));
 
 1;
+
+# ABSTRACT: Model class for the GeoIP Precision Country end point
+
+__END__
+
+=head1 SYNOPSIS
+
+  use v5.10;
+
+  my $client = GeoIP2::Webservice::Client->new(
+      user_id     => 42,
+      license_key => 'abcdef123456',
+  );
+
+  my $country = $client->country( ip => '24.24.24.24' );
+
+  my $country_rec = $country->country();
+  say $country_rec->iso_3166_alpha_2();
+
+=head1 DESCRIPTION
+
+This class provides a model for the data returned by the GeoIP Precision
+Country end point.
+
+=head1 METHODS
+
+This class provides the following methods, each of which returns a record
+object.
+
+=head2 $country->continent()
+
+Returns a L<GeoIP2::Record::Continent> object representing continent data for
+the requested IP address.
+
+=head2 $country->country()
+
+Returns a L<GeoIP2::Record::Country> object representing country data for the
+requested IP address. This record represents the country where the GeoIP
+service believes the IP is located in.
+
+=head2 $country->registered_country()
+
+Returns a L<GeoIP2::Record::Country> object representing the registered
+country data for the requested IP address. This record represents the country
+where the ISP has registered a given IP block in, and may differ from the
+user's country.
+
+=head2 $country->traits()
+
+Returns a L<GeoIP2::Record::Traits> object representing the traits for the
+request IP address.
--- lib/GeoIP2/Model/Omni.pm
+++ lib/GeoIP2/Model/Omni.pm
@@ -14,3 +14,73 @@ __PACKAGE__->_define_attributes_for_keys(
     qw( city continent country location region registered_country traits ));
 
 1;
+
+# ABSTRACT: Model class for the GeoIP Precision Omni end point
+
+__END__
+
+=head1 SYNOPSIS
+
+  use v5.10;
+
+  my $client = GeoIP2::Webservice::Client->new(
+      user_id     => 42,
+      license_key => 'abcdef123456',
+  );
+
+  my $omni = $client->city_isp_org( ip => '24.24.24.24' );
+
+  my $city_rec = $omni->city();
+  say $city_rec->name();
+
+=head1 DESCRIPTION
+
+This class provides a model for the data returned by the GeoIP Precision Omni
+end point.
+
+The only difference between the City, City/ISP/Org, and Omni model classes is
+which fields in each record may be populated. See
+http://dev.maxmind.com/geoip/precision for more details.
+
+=head1 METHODS
+
+This class provides the following methods, each of which returns a record
+object.
+
+=head2 $country->city()
+
+Returns a L<GeoIP2::Record::City> object representing country data for the
+requested IP address.
+
+=head2 $country->continent()
+
+Returns a L<GeoIP2::Record::Continent> object representing continent data for
+the requested IP address.
+
+=head2 $country->country()
+
+Returns a L<GeoIP2::Record::Country> object representing country data for the
+requested IP address. This record represents the country where the GeoIP
+service believes the IP is located in.
+
+=head2 $country->location()
+
+Returns a L<GeoIP2::Record::Location> object representing country data for the
+requested IP address.
+
+=head2 $country->region()
+
+Returns a L<GeoIP2::Record::Region> object representing country data for the
+requested IP address.
+
+=head2 $country->registered_country()
+
+Returns a L<GeoIP2::Record::Country> object representing the registered
+country data for the requested IP address. This record represents the country
+where the ISP has registered a given IP block in, and may differ from the
+user's country.
+
+=head2 $country->traits()
+
+Returns a L<GeoIP2::Record::Traits> object representing the traits for the
+request IP address.
