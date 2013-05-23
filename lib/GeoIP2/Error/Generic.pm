46892527a7df616b75589c345362e46ebcf95f53
Dave Rolsky <autarch@urth.org>
2013-05-23 14:55:17 -0500

    Remove some unneeded module loads

--- lib/GeoIP2/Error/Generic.pm
+++ lib/GeoIP2/Error/Generic.pm
@@ -3,8 +3,6 @@ package GeoIP2::Error::Generic;
 use strict;
 use warnings;
 
-use GeoIP2::Types qw( Str );
-
 use Moo;
 
 extends 'Throwable::Error';
--- lib/GeoIP2/Error/HTTP.pm
+++ lib/GeoIP2/Error/HTTP.pm
@@ -3,8 +3,6 @@ package GeoIP2::Error::HTTP;
 use strict;
 use warnings;
 
-use GeoIP2::Types qw( Str );
-
 use Moo;
 
 with 'GeoIP2::Role::Error::HTTP';
