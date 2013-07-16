b44b1e02117e512690f06bae371481a74ac0ad97
Dave Rolsky <autarch@urth.org>
2013-07-16 12:11:04 -0500

    Make synopsis indentation consistent

--- lib/GeoIP2/Database/Reader.pm
+++ lib/GeoIP2/Database/Reader.pm
@@ -101,18 +101,18 @@ __END__
 
 =head1 SYNOPSIS
 
-    use 5.008;
+  use 5.008;
 
-    use GeoIP2::Database::Reader;
+  use GeoIP2::Database::Reader;
 
-    my $reader = GeoIP2::Database::Reader->new(
-        file      => '/path/to/database',
-        languages => [ 'en', 'de', ]
-    );
+  my $reader = GeoIP2::Database::Reader->new(
+      file      => '/path/to/database',
+      languages => [ 'en', 'de', ]
+  );
 
-    my $omni = $reader->omni( ip => '24.24.24.24' );
-    my $country = $omni->country();
-    say $country->is_code();
+  my $omni = $reader->omni( ip => '24.24.24.24' );
+  my $country = $omni->country();
+  say $country->is_code();
 
 =head1 DESCRIPTION
 
